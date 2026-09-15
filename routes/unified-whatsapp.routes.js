6a8e2b19ddb43ee3776b8d6a3d83ee942b414548
mahendra23081999 <148061114+mahendra23081999@users.noreply.github.com>
2026-09-15 15:30:14 +0530

    Update unified-whatsapp.routes.js

--- routes/unified-whatsapp.routes.js
+++ routes/unified-whatsapp.routes.js
@@ -36,6 +36,6 @@ router.get('/waba-list', checkPermission('view.unified_whatsapp'), unifiedWhatsA
 router.get('/phone-numbers', checkPermission('view.unified_whatsapp'), unifiedWhatsAppController.getMyPhoneNumbers);
 router.put('/phone-numbers/:phoneNumberId/set-primary', checkPermission('update.unified_whatsapp'), unifiedWhatsAppController.setPrimaryPhoneNumber);
 router.get('/:wabaId/phone-numbers', checkPermission('view.unified_whatsapp'), unifiedWhatsAppController.getWabaPhoneNumbers);
-router.post('/embedded-signup/connection', checkPermission('create.unified_whatsapp'), unifiedWhatsAppController.getEmbbededSignupConnection);
+router.post('/embedded-signup/connection', unifiedWhatsAppController.getEmbbededSignupConnection);
 router.get('/contact-profile', checkPermission('view.unified_whatsapp'), unifiedWhatsAppController.getContactProfile);
 export default router;
